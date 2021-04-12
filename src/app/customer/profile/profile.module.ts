7c63ad0492b7e8f47aca5dabcef39d242cf7ecae
Hrabi80 <hrabi.ahmed88@gmail.com>
2021-04-12 10:29:52 +0100

    12apr

---
+++ src/app/customer/profile/profile.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactPanelComponent } from './contact-panel/contact-panel.component';
+
+
+
+@NgModule({
+  declarations: [ContactPanelComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProfileModule { }
