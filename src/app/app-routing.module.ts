790e4b9b6e6ee4bedc55da513f239e1d272eccd7
Hrabi80 <hrabi.ahmed88@gmail.com>
2021-04-12 12:03:53 +0100

    nothing special

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 import { LandingPageComponent } from './public/landing-page/landing-page.component';
+import { ProfileComponent } from './public/profile/profile.component';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
   {path: 'home', component: LandingPageComponent},
+  {path: 'test', component: ProfileComponent},
   {path:'profile', loadChildren: () => import('./public/profile/profile.module')
   .then(m => m.ProfileModule)}
 ];
